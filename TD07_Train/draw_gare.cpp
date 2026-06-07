9e23663be4ad9c63d54151953f3b4f6a3899e859
ben.brl <benoitb21@icloud.com>
2026-06-07 10:14:13 +0200

    [WIP] Dino Gare 🚉

--- TD07_Train/draw_gare.cpp
+++ TD07_Train/draw_gare.cpp
@@ -46,34 +46,93 @@ void drawToit(float dim_gare, float angle){
 	triangle.drawShape();
 	myEngine.mvMatrixStack.popMatrix();
 
+void drawEntree()
+{
+	float taille_pilone = 1;
+	float hauteur_pilone = 5;
 
+	// pylone 1
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.mvMatrixStack.addTranslation(Vector3D(0, taille_pilone / 2.0f, hauteur_pilone / 2.0f));
+	myEngine.mvMatrixStack.addHomothety(Vector3D(taille_pilone, taille_pilone, hauteur_pilone));
+	myEngine.setFlatColor(0.643, 0.447, 0.318);
+	myEngine.updateMvMatrix();
+	cube->draw();
+	myEngine.mvMatrixStack.popMatrix();
 
+	// pylone 2
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.mvMatrixStack.addTranslation(Vector3D(0, 10 - taille_pilone + taille_pilone / 2.f, hauteur_pilone / 2.0f));
+	myEngine.mvMatrixStack.addHomothety(Vector3D(taille_pilone, taille_pilone, hauteur_pilone));
+	myEngine.setFlatColor(0.643, 0.447, 0.318);
+	myEngine.updateMvMatrix();
+	cube->draw();
+	myEngine.mvMatrixStack.popMatrix();
+
+	// auvent
+	float largeur_auvent = 4.f;
+	float hauteur_auvent = 1.f;
+	float longeur_auvent = 10.f;
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.mvMatrixStack.addTranslation(Vector3D(-hauteur_auvent - taille_pilone / 2, longeur_auvent / 2.0f, hauteur_pilone + hauteur_auvent / 2.f));
+	myEngine.mvMatrixStack.addHomothety(Vector3D(largeur_auvent, longeur_auvent, hauteur_auvent));
+	myEngine.setFlatColor(0.643, 0.447, 0.318);
+	myEngine.updateMvMatrix();
+	cube->draw();
+	myEngine.mvMatrixStack.popMatrix();
+
+	// rotation pylone 1
+	float hauteur_pilone_rotation = 3.7f;
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.mvMatrixStack.addTranslation(Vector3D(0, taille_pilone + 1.0f, hauteur_pilone - 1.0f));
+	myEngine.mvMatrixStack.addRotation(deg2rad(-45), Vector3D(1, 0, 0));
+	myEngine.mvMatrixStack.addHomothety(Vector3D(taille_pilone, taille_pilone, hauteur_pilone_rotation));
+	myEngine.setFlatColor(0.643, 0.447, 0.318);
+	myEngine.updateMvMatrix();
+	cube->draw();
+	myEngine.mvMatrixStack.popMatrix();
+
+	// rotation pylone 2
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.mvMatrixStack.addTranslation(Vector3D(0, 10 - taille_pilone - 1.0f, hauteur_pilone - 1.0f));
+	myEngine.mvMatrixStack.addRotation(deg2rad(45), Vector3D(1, 0, 0));
+	myEngine.mvMatrixStack.addHomothety(Vector3D(taille_pilone, taille_pilone, hauteur_pilone_rotation));
+	myEngine.setFlatColor(0.643, 0.447, 0.318);
+	myEngine.updateMvMatrix();
+	cube->draw();
+	myEngine.mvMatrixStack.popMatrix();
 }
 
-
-
-void drawGare(){
-	//murs
-	float angle = M_PI/6.0f;
-	
+void drawGare()
+{
+	float angle = M_PI / 6.0f;
 	float dim_gare = 10.0f;
+
+	float hauteur_gare = 6.f;
+	float largeur_gare = 4;
+
+	// murs
 	myEngine.mvMatrixStack.pushMatrix();
-	myEngine.mvMatrixStack.addTranslation(Vector3D(dim_gare/2.0f, dim_gare/2.0f,dim_gare/2.0f));
-	myEngine.mvMatrixStack.addHomothety(Vector3D(dim_gare, dim_gare, dim_gare));
+	myEngine.mvMatrixStack.addTranslation(Vector3D(largeur_gare / 2.0, dim_gare / 2.f, hauteur_gare / 2.f));
+	myEngine.mvMatrixStack.addHomothety(Vector3D(largeur_gare, dim_gare, hauteur_gare));
 	myEngine.updateMvMatrix();
 	myEngine.setFlatColor(0.549, 0.353, 0.235);
 	rail->draw();
 	myEngine.mvMatrixStack.popMatrix();
 
-	//toit
-	drawToit(dim_gare, angle);
-	
+	// toit
+	myEngine.updateMvMatrix();
 	myEngine.mvMatrixStack.pushMatrix();
-	myEngine.mvMatrixStack.addRotation( M_PI/2.0f, Vector3D(0,0,1));
-	float d = dim_gare/3.0f;
-	myEngine.mvMatrixStack.addTranslation(Vector3D(0,-dim_gare, d));
-	//myEngine.mvMatrixStack.addHomothety(Vector3D(1, 1, dim_gare));
-	float dim{};
+	drawToit(dim_gare, angle);
+
+	myEngine.mvMatrixStack.popMatrix();
+
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.mvMatrixStack.addTranslation(Vector3D(7.5, 0, 0));
+	myEngine.updateMvMatrix();
+	drawEntree();
+	myEngine.mvMatrixStack.popMatrix();
+}
 
 void drawGare_position(Position position)
 {
